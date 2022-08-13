4a4f537f6a07d085d73e66525b5d8766ccbfd67f
Ishaan Subramanya <whthownothing@gmail.com>
2022-08-13 10:38:09 +0530

    built ci

--- ci.sh
+++ ci.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-sudo apt install -y build-essential curl file git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin/:$PATH"' >> $BASH_ENV
-echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >> $BASH_ENV
-echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >> $BASH_ENV
-brew install gh
+#sudo apt-get install -y build-essential curl file git
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+#echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin/:$PATH"' >> $BASH_ENV
+#echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >> $BASH_ENV
+#echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >> $BASH_ENV
+#brew install gh
 cargo build --release
+if [ $? != 0 ]; then
+    exit $?
+fi
 cd target/release
-tar cjf homebrew-pck.tar.bz2 rc_bin
-gh release create latest homebrew-pck.tar.bz2
+tar -cjf homebrew-pck.tar.bz2 rc_bin
+cd ../../
+VER=$(sed -ne 's/version\s?*=\s?*\"\(.*\)\"/\1/p' ./Cargo.toml)
+gh release create $VER \
+./target/release/homebrew-pck.tar.bz2 \
+--generate-notes
